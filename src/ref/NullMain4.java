39de4e2830c9957826206a016f87fc242505ac7b
seosunggwan <suck6041@gmail.com>
2024-03-04 12:53:24 +0900

    수정

--- src/ref/NullMain4.java
+++ src/ref/NullMain4.java
@@ -8,5 +8,6 @@ public class NullMain4 {
         System.out.println("bigData.count" + bigData.count);
         System.out.println("bigData.data" + bigData.data);
         System.out.println("bigData.data.value" + bigData.data.value);
+        System.out.println("하잉");
     }
 }
